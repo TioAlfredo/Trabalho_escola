f1fc5b30e5d0036215d28b9b41304ee0581012c9
TioAlfredo <58608742+TioAlfredo@users.noreply.github.com>
2019-12-06 17:02:12 -0300

    Add files via upload

---
+++ cad_aluno_turma.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css"  href="estilos.css" />
+<title>cad_aluno</title>
+</head>
+
+<div id="fundo">
+<body>
+<div id="form">
+<form action="/pagina-processa-dados-do-form" method="post">
+<div>
+<img src="imgs/1486395884-account_80606.png" alt="0" width="44px" height="44px" />
+</div>
+    <div>
+        <label for="nome">Número da turma</label>
+        <input type="text" id="nome" />
+    </div>
+    <div>
+        <label for="email">Descrição:</label>
+        <input type="email" id="email" />
+    </div>
+    <div>
+      <label for="msg">Sexo:</label>
+        <input type="text" id="ID" value="" maxlength="44" />
+    </div>
+    <div>
+      <label for="name">Quantidade de vagas</label>
+        <input type="text" id="name" size="20" />
+        </div>
+        <div>
+      <label for="name">Nome do Professor</label>
+        <input type="text" id="name" size="20" />
+        </div>
+    
+    
+</form>
+
+<form action="/pagina-processa-dados-do-form" method="post">
+    
+    
+    <div class="button">
+        <button type="submit">Cadastrar</button>
+        <button type="submit">Limpa</button>
+    </div>
+    
+</form>
+
+</div>
+</body>
+</html>
---
+++ cad_turma.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css"  href="estilos.css" />
+<title>cad_aluno</title>
+</head>
+
+<div id="fundo">
+<body>
+<div id="form">
+<form action="/pagina-processa-dados-do-form" method="post">
+<div>
+<img src="imgs/1486395884-account_80606.png" alt="0" width="44px" height="44px" />
+</div>
+    <div>
+        <label for="nome">Matrícula:</label>
+        <input type="text" id="nome" />
+    </div>
+    <div>
+        <label for="email">Nome:</label>
+        <input type="email" id="email" />
+    </div>
+    <div>
+      <label for="msg">Sexo:</label>
+        <input type="text" id="ID" value="" maxlength="44" />
+    </div>
+    <div>
+      <label for="name">Data de nascimento:</label>
+        <input type="text" id="name" size="20" />
+    </div>
+    
+    
+</form>
+
+<form action="/pagina-processa-dados-do-form" method="post">
+    
+    
+    <div class="button">
+        <button type="submit">Cadastrar</button>
+        <button type="submit">Limpa</button>
+    </div>
+    
+</form>
+
+</div>
+</body>
+</html>
